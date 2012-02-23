69599cea7880b0977bf91938eb00a033b40d4819
Zenobius Jiricek <zeno.jiricek@urpages.net>
2012-02-23 14:37:34 +1030

    Merge conflict fix

--- cmsplugin_embeddedpages/forms.py
+++ cmsplugin_embeddedpages/forms.py
@@ -1,11 +1,5 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, ModelChoiceField, ChoiceField
 from django.utils.safestring import SafeString
-<<<<<<< HEAD
-
-from .models import (
-  EmbedPagesPlugin,
-=======
-from django.forms import ModelChoiceField, ChoiceField
 
 from .lib.choices import (
   DynamicTemplateChoices,
@@ -16,26 +10,11 @@ from .models import (
   TEMPLATE_PATH,
   GROUP_TEMPLATE_PATH,
   PAGE_TEMPLATE_PATH,
->>>>>>> b1cbd94f3893f05a316f8f78d619677c297adeb1
 )
 
 
 class PagePluginAdminForm(ModelForm):
 
-<<<<<<< HEAD
-    class Meta:
-        model = EmbedPagesPlugin
-
-    def __init__(self, *args, **kwargs):
-        super(EmbedPagesAdminForm, self).__init__(*args, **kwargs)
-        choices = [self.fields['root'].choices.__iter__().next()]
-        for page in self.fields['root'].queryset:
-            choices.append(
-                (page.id,
-                 SafeString(''.join([u"&nbsp;&nbsp;&nbsp;"*page.level,
-                   page.__unicode__()]))))
-
-=======
     class Meta :
         model = PagePluginSettings
 
@@ -59,5 +38,4 @@ class PagePluginAdminForm(ModelForm):
                  SafeString(''.join([u"&nbsp;&nbsp;&nbsp;"*page.level,
                    page.__unicode__()]))))
 
->>>>>>> b1cbd94f3893f05a316f8f78d619677c297adeb1
         self.fields['root'].choices = choices
